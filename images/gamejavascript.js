feb08096c4d8db8ab478ece5dea58e7ad4f6c8e3
akki <akki@localhost.localdomain>
2016-05-13 17:31:02 +0530

    flag images

---
+++ images/gamejavascript.js
@@ -0,0 +1,507 @@
+
+ 
+
+ 
+/* declaring an array to hold the country names */
+ 
+ var country = new Array(70);
+ var flag = new Array(70);
+ var imageKey=0;
+ 
+
+/*my countries value are present here*/
+ 
+   country[0] ='AFGHANISTAN';//333
+   flag[0]='afghanistan.png';
+   
+   
+   
+				   
+				   
+   //country[1] = "AFRICA";
+   //country[2] = "ALBANIA";
+   //country[3] = "ALGERIA";
+   //country[4] = "AMERICA";
+   //country[5] = "ANDORRA";
+   country[1] = "ANGOLA";//412
+   flag[1]='angola.png';
+   country[2] = "ANTARCTICA";//453
+   flag[2]="antartica.png";
+   //country[8] = "ARGENTINA";
+   country[3] = "ARMENIA";//352
+   flag[3]="armenia.png"
+  //country[10] = "AUSTRALIA";
+  //country[11] = "AUSTRIA";
+  country[4] = "AZERBAIJAN";//535
+  flag[4]="azerbaijan.png";
+  country[5] = "BAHAMAS";//611
+  flag[5]="bahamas.png";
+  country[6] = "BAHRAIN";//644
+  flag[6]="bahrain.png";
+  //country[15] = "BANGLADESH";
+  country[7] = "BARBADOS";//716
+  flag[7]="barbados.png";
+  country[8] = "BELARUS";//755
+  flag[8]="belarus.png";
+  //country[18] = "BELGIUM";
+  country[9] = "BELIZE";//858
+  flag[9]="belize.png";
+  country[10] = "BENIN";//940
+  flag[10]="benin.png";
+  //country[21] = "BHUTAN";
+  //country[22] = "BOLIVIA";
+  //country[23] = "BOSNIA HERZEGOVINA";
+  //country[24] = "BOTSWANA";
+  //country[25] = "BRAZIL";
+  //country[26] = "BRUNEI";
+  //country[27] = "BULGARIA";
+  //country[28] = "BURKINA";
+  //country[29] = "BURUNDI";
+  country[11] = "CAMBODIA";//016
+  flag[11]="cambodia.png";
+  //country[31] = "CAMEROON";
+  //country[32] = "CANADA";
+  //country[33] = "CAPE VERDE ISLANDS";
+  country[12] = "CHAD";//153
+  flag[12]="chad.png";
+  //country[35] = "CHILE";
+  //country[36] = "CHINA";
+  //country[37] = "COLOMBIA";
+  country[13] = "COMOROS";//239
+  flag[13]="comoros.png";
+  country[14] = "CONGO";//315
+  flag[14]="congo.png";
+  
+  //country[40] = "COSTA RICA";
+  //country[41] = "CROATIA";
+  //country[42] = "CUBA";
+  country[15] = "CYPRUS";//437
+  flag[15]="cyprus.png";
+  //country[44] = "CZECH REPUBLIC";
+  //country[45] = "DENMARK";
+  country[16] = "DJIBOUTI";//526
+  flag[16]="djibouti.png";
+  //country[47] = "DOMINICAN REPUBLIC";
+  country[17] = "ECUADOR";//609
+  flag[17]="ecuador.png";
+  //country[49] = "EGYPT";
+  //country[50] = "EL SALVADOR";
+  country[18] = "ERITREA";//652
+  flag[18]="eritrea.png";
+  country[19] = "ESTONIA";//741
+  flag[19]="estonia.png";
+  country[20] = "ETHIOPIA";//822
+  flag[20]="ethiopia.png";
+  //country[54] = "EUROPE";
+  country[21] = "FIJI";//858
+  flag[21]="fiji.png";
+  country[22] = "FINLAND";//941
+  flag[22]="finland.png";
+ // country[57] = "FRANCE";
+  country[23] = "GABON";//026
+  flag[23]="gabon.png";
+  country[24] = "GAMBIA";//134
+  flag[24]="gambia.png";
+  country[25] = "GEORGIA";//247
+  flag[25]="georgia.png";
+  //country[61] = "GERMANY";
+  country[26] = "GHANA";//319
+  flag[26]="ghana.png";
+  //country[63] = "GREECE";
+  country[27] ="GRENADA";//401
+  flag[27]="grenada.png";
+  country[28] = "GUATEMALA";//504
+  flag[28]="guatemala.png";
+  //country[66] = "GUINEA";
+  country[29] = "HAITI";//540
+  flag[29]="haiti.png";
+  
+  //country[68] = "HOLLAND";
+  country[30] = "HONDURAS";//610
+  flag[30]="honduras.png";
+  //country[70] = "HONG KONG";
+  country[31] = "HUNGARY";//645
+  flag[31]="hungary.png";
+  country[32] = "ICELAND";//720
+  flag[32]="iceland.png";
+  //country[73] = "INDIA";
+  country[33] = "INDONESIA";//757
+  flag[33]="indonesia.png";
+  country[34] = "IRAN";//826
+  flag[34]="iran.png";
+  country[35] = "IRAQ";//856
+  flag[35]="iraq.png";
+  //country[77] = "ISRAEL";
+  //country[78] = "ITALY";
+  country[36] = "JAMAICA";//929
+  flag[36]="jamaica.png";
+  //country[80] = "JAPAN";
+  country[37] = "JORDAN";//021
+  flag[37]="jordan.png";
+  country[38] = "KAZAKHSTAN";//049
+  flag[38]="kazakhstan.png";
+  //country[83] = "KENYA";
+  country[39] = "KIRGYZSTAN";//125
+  flag[39]="kirgyzstan.png";
+  country[40] = "KIRIBATI";//152
+  flag[40]="kiribati.png";
+  //country[86] = "KOREA";
+  //country[87] = "KUWAIT";
+  country[41] = "LAOS";//230
+  flag[41]="laos.png";
+  country[42] = "LATVIA";//304
+  flag[42]="latvia.png";
+  country[43] = "LEBANON";//336
+  flag[43]="lebanon.png";
+  country[44] = "LESOTHO";//407
+  flag[44]="lesotho.png";
+  //country[92] = "LIBERIA";
+  //country[93] = "LIBYA";
+  //country[94] = "LIECHTENSTEIN";
+  //country[95] = "LITHUANIA";
+  //country[96] = "LUXEMBOURG";
+  country[45] = "MADAGASCAR";//445
+  flag[45]="madagascar.png";
+  //country[98] = "MALAWI";
+  country[46] = "MALAYSIA";//515
+  flag[46]="malaysia.png";
+ country[47] = "MALDIVES";//548
+ flag[47]="maldives.png";
+ //country[101] = "MALI";
+ country[48] = "MALTA";//617
+ flag[48]="malta.png";
+ //country[103] = "MAURITANIA";
+ //country[104] = "MAURITIUS";
+ country[49] = "MEXICO";//656
+ flag[49]="mexico.png";
+ //country[106] = "MOLDOVA";
+ country[50] = "MONACO";//728
+ flag[50]="monaco.png";
+ //country[108] = "MONGOLIA";
+ //country[109] = "MONTSERRAT";
+ //country[110] = "MOROCCO";
+ //country[111] = "MOZAMBIQUE";
+ //country[112] = "MYANMAR";
+ //country[113] = "NAMIBIA";
+ //country[114] = "NAURU";
+ //country[115] = "NEPAL";
+ country[51] = "NETHERLANDS";//818
+ flag[51]="netherlands.png";
+ //country[117] = "NEW ZEALAND";
+ //country[118] = "NICARAGUA";
+ //country[119] = "NIGERIA";
+ country[52] = "NORWAY";//853
+ flag[52]="norway.png";
+ country[53] = "OMAN";//921
+ flag[53]="oman.png";
+ //country[122] = "PAKISTAN";
+ //country[123] = "PANAMA";
+ //country[124] = "PAPUA NEW GUINEA";
+ country[54] = "PARAGUAY";//953
+ flag[54]="paraguay";
+ country[55] = "PERU";//034
+ flag[55]="peru.png";
+ //country[127] = "PHILIPPINES";
+ country[56] = "POLAND";//117
+ flag[56]="poland.png";
+ //country[129] = "PORTUGAL";
+ country[57] = "QATAR";//214
+ flag[57]="qatar.png";
+ //country[131] = "ROMANIA";
+ country[58] = "RUSSIA";//246
+ flag[58]="russia.png";
+ //country[133] = "RWANDA";
+ //country[134] = "SAN MARINO";
+ country[59] = "SAUDI ARABIA";//323
+ flag[59]="saudiarabia.png";
+ //country[136] = "SENEGAL";
+ //country[137] = "SEYCHELLES";
+ //country[138] = "SIERRA LEONE";
+ country[60] = "SINGAPORE";//445
+ flag[60]="singapore.jpg";
+ //country[140] = "SLOVAKIA";
+ //country[141] = "SLOVENIA";
+ //country[142] = "SOLOMON ISLANDS";
+ //country[143] = "SOMALIA";
+ //country[144] = "SOUTH AFRICA";
+ //country[145] = "SPAIN";
+ //country[146] = "SRI LANKA";
+ //country[147] = "SUDAN";
+ //country[148] = "SURINAM";
+ //country[149] = "SWAZILAND";
+ country[61] = "SWEDEN";//540
+ flag[61]="sweden.png";
+ country[62] = "SWITZERLAND";//614
+ flag[62]="switzerland.png";
+ country[63] = "SYRIA";//643
+ flag[63]="syria.png";
+ //country[153] = "TAIWAN";
+ //country[154] = "TAJIKITAN";
+ //country[155] = "TANZANIA";
+ country[64] = "THAILAND";//715
+ flag[64]="thailand.png";
+ //country[157] = "TOGO";
+ //country[158] = "TONGA";
+ //country[159] = "TRINIDAD";
+ //country[160] = "TUNISIA";
+ //country[161] = "TURKEY";
+ //country[162] = "TURKMENISTAN";
+ //country[163] = "TUVALU";
+ //country[164] = "UGANDA";
+ //country[165] = "UKRAINE";
+ country[65] = "UNITED ARAB EMIRATES";//756
+ flag[65]="unitedarabemirates.png";
+ country[66] = "URUGUAY";//823
+ flag[66]="uruguay.png";
+ //country[168] = "UZBEKISTAN";
+ //country[169] = "VANUATU";
+ //country[170] = "VATICAN CITY";
+ //country[171] = "VENEZUELA";
+ country[67] = "VIETNAM";//858
+ flag[67]="vietnam.png";
+ //country[173] = "WEST INDIES";
+ //country[174] = "WESTERN SAMOA";
+ //country[175] = "YEMEN REPUBLIC";
+ country[68] = "YUGOSLAVIA";//935
+ flag[68]="yugoslavia.png";
+ country[69] = "ZAIRE";//013
+ flag[69]="zaire.png";
+ //country[178] = "ZAMBIA";
+ //country[179] = "ZIMBABWE";
+ 
+/* generates a random integer between 0 to 179 */
+ 
+ var key = Math.floor(Math.random() * 70);
+ 
+/* stores the country name */
+ var temp = country[key];
+ 
+ 
+/* declaring and initializing tries */
+ 
+ var tries = 0;
+ 
+/* the main function */
+
+function helpMe()
+{
+
+	 var counter;
+
+  switch(tries)
+   {
+ 
+     case 1:
+     //document.form1.hint.value = "First Hint : The country name starts with " + temp.charAt(0);
+	 window.alert("First Hint : The country name starts with " + temp.charAt(0));
+     break;
+ 
+     case 2:
+     //document.form1.hint.value = "Second Hint : The country name ends with " + temp.charAt(temp.length - 1);
+	 window.alert("Second Hint : The country name ends with " + temp.charAt(temp.length - 1));
+     break;
+ 
+     case 3:
+    // document.form1.hint.value = "Last Hint : The country name has " + temp.length + " characters";
+	window.alert("Third Hint : The country name has " + temp.length + " characters");
+     break;
+ 
+     
+	 case 4:
+	  counter=(Math.ceil(((temp.length)/2)));
+	 window.alert("Fourth Hint : The country " +(counter+1)+" alphabet is " + temp.charAt(counter));
+     break;
+     default:
+     //document.form1.hint.value = "No hints are available";
+	 window.alert("No hints are available");
+ 
+   }
+ 
+  
+  
+}
+ 
+ function guessit()
+ {
+	 /*if(counter!0)
+	 {
+ key = Math.floor(Math.random() * 70);
+ 
+
+  temp = country[key];
+ 
+ 
+ 
+  tries = 0;
+ 	
+	 }*/
+	 
+	 
+ ++imageKey;
+   var guess = document.form1.guess1.value;
+//    document.write("the value u entered is "+guess);
+   tries++;
+   var change=document.getElementById("img1");   
+   change.src=flag[key];
+  /*  switch(tries)
+   {
+ 
+     case 1:
+     document.form1.hint.value = "First Hint : The country name starts with " + temp.charAt(0);
+     break;
+ 
+     case 2:
+     document.form1.hint.value = "Second Hint : The country name ends with " + temp.charAt(temp.length - 1);
+     break;
+ 
+     case 3:
+     document.form1.hint.value = "Last Hint : The country name has " + temp.length + " characters";
+     break;
+ 
+     default:
+     document.form1.hint.value = "No hints are available";
+ 
+   }
+   */
+ 
+	 
+
+ 
+
+ 
+   if(guess.toUpperCase()!= temp)  /* if guess not equals to temp */
+   {
+   
+      window.alert("Nope!that is nowhere close..think more"); 
+ 
+   }
+   
+   if(guess.toUpperCase() == temp)  /* if guess equals to temp */
+   {
+   
+ 
+    if(window.confirm("Absolutely Right ! Yes the country was " + temp + "\nDo you want to play again?"))
+	{
+		
+    window.location.reload();     /* reloads the page for a new game */
+	}
+ 
+    
+      
+   }
+ 
+   else
+   {
+ 
+     if(tries == 5) /* game over */
+     {
+ 
+    if(window.confirm("Sorry ! Your chances over. The country was  " + temp + "\nDo you want to play again?"))
+    {
+      window.location.reload(); /* reloads the page for a new game */
+ 
+      //document.form1.hint.value = "Enter your guess below and click on Check!";
+ 
+    }
+ 
+     }
+ 
+   }
+
+changeBackImage();
+ }
+ 
+
+ 
+
+ 
+ function catchKeyCode() //calls when the user press the RETURN key
+ {
+ 
+   if(event.keyCode == 13||event.which==13)
+    guessit();
+   
+   if(event.keycode==124||event.which==124)
+	   
+	   {
+		   //window.alert("i got active");
+		   helpIt();   
+		   
+	   }
+	   if(event.keycode==43||event.which==43)
+	   {
+		   helpMe()
+	   }
+ }
+ var playerName;
+ 
+ function startMe()
+ {
+ 
+// var window1=window.open("","","width=300,height=200");
+   var playerName=window.prompt("WHAT SHOULD I CALL YOU?");
+   var elementi=document.getElementById("hello");
+   elementi.value="Hii!  "+ playerName;
+   var window1=window.open("rules.html","rules","width=300,height=200");
+   
+  // document.body.style.backgroundImage="url('world1.jpg')";
+   
+ 
+ }
+ 
+ function changeBackImage()
+ {
+//	   var imageKey = Math.floor(Math.random() * 6);
+   //document.write(imageKey);
+   switch(imageKey)
+   {
+ 
+     case 1:
+     document.body.style.backgroundImage="url('world1.jpg')";
+     break;
+ 
+     case 2:
+     document.body.style.backgroundImage="url('world10.jpg')";
+     break;
+ 
+     case 3:
+     document.body.style.backgroundImage="url('world11.jpg')";
+     break;
+ 
+     case 4:
+	 document.body.style.backgroundImage="url('world12.jpg')";
+	 
+	 case 5:
+	 document.body.style.backgroundImage="url('world5.jpg')";
+	 
+	 
+     default:
+     document.body.style.backgroundImage="url('world6.jpg')";
+ 
+   }
+ 
+ }
+ 
+ function helpIt()
+ {
+	 window.alert(temp);
+ }
+ 
+ 
+ function newGame()
+ {
+ 
+   if(window.confirm("Do you want to start a new game?"))
+   {
+ 
+       window.location.reload();  /* reloads the page for a new game */
+ 
+  //   document.form1.hint.value = "Enter your guess below and click on Guess!";
+  
+  
+ 
+   }
+ 
+ }
+ 
+ 
